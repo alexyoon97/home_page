81b162fd45917eb85c8519661ddede35861ac262
Alex Yoon <wonsangalex@gmail.com>
2020-10-06 21:31:41 -0700

    minor change

--- src/routes/About.js
+++ src/routes/About.js
@@ -19,22 +19,18 @@ function About() {
 
           <p>
             I studied Computer Engineering for 2 years at NAIT, my course
-            focused <img className="Nait" src={Nait}></img> learning a
-            Object-Oriented Programming, C and C++, Web
-            developement and Hardware Interfacing. I am a person that like to
-            interact with new challenges and enjoy to learn and pick up new
-            skills to improve more.
+            focused <img className="Nait" src={Nait}></img> learning
+            Object-Oriented Programming C#, C and C++, Web development and Hardware Interfacing. I am a person that likes to interact with new challenges and enjoy learning and pick up new skills to improve more.
             <br></br>
-            <br></br>Whenever I face a difficulty or challenges I think as it's great time to improve 
-            and find critical weakness of my self. 
-            I re-energize by talk and meeting with people or playing active and social sports.
+            <br></br>Whenever I face a difficulty or challenges I think as it's a great time to improve 
+            and find a critical weakness of myself. 
+            I re-energize my-self by talk and meeting with people or playing active and social sports.
             <br></br>
             <br></br>I achieved a Computer Engineering Degree in 2019 and
             currently I am studying more on Front-End side to
-            expand my expertise because I am intrested in designing visuals and 
+            expand my expertise because I am interested in designing visuals and 
             develop User Interfaces. I hope you enjoyed my website and 
-            feel free to reach me out for any types of conversation to
-            wonsangalex@gmail.com.
+            feel free to reach me out for any type of conversation.
           </p>
           <div className="hrefs">
             <a href="https://www.linkedin.com/in/alex-yoon-0793641a5/" target="_blank">Linkedin</a>
