fbcf2e643bc28c7995ec0d5ab061ffc816128a4f
Alex Yoon <wonsangalex@gmail.com>
2020-12-13 23:56:19 -0800

    imgslider edited

--- src/components/Portfo_Component.js
+++ src/components/Portfo_Component.js
@@ -12,9 +12,9 @@ function Portfo_Component({ Portfolio_list }) {
   }
   //spring style options
   const transitions = useTransition(index, (p) => p, {
-    from: { opacity: 0, transform: "translate3d(100%,0,0)" },
+    from: { opacity: 0, transform: "translate3d(-70%,0,0)" },
     enter: { opacity: 1, transform: "translate3d(0%,0,0)" },
-    leave: { opacity: 0, transform: "translate3d(-50%,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(50%,0,0)" },
   });
   return (
     //page instructor line 20 to 25
